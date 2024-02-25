89cd20de1e4d1e9abf6478d254c95533e8df0eee
CoCoMilkyWay <wangchuyin980321@gmail.com>
2024-02-26 02:38:03 +0800

    commit

--- demos/cta_stk_bt/runBT.py
+++ demos/cta_stk_bt/runBT.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     #创建一个运行环境，并加入策略
     engine = WtBtEngine(EngineType.ET_CTA)
     engine.init(folder='../common/', cfgfile="configbt.yaml", commfile="stk_comms.json", contractfile="stocks.json")
-    engine.configBacktest(201901010930,201912151500)
-    engine.configBTStorage(mode="csv", path="../storage/")
+    #engine.configBacktest(201901010930,201912151500)
+    #engine.configBTStorage(mode="csv", path="../storage/")
     engine.commitBTConfig()
     
     straInfo = StraDualThrust(name='pydt_SH510300', code="SSE.ETF.510300", barCnt=50, period="m5", days=30, k1=0.1, k2=0.1)
