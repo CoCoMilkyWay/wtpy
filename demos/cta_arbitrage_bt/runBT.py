3ea8dd95a3d242b903c6857ca84ee4278040a569
CoCoMilkyWay <wangchuyin980321@gmail.com>
2024-07-24 12:24:40 +0000

    cmt

--- demos/cta_arbitrage_bt/runBT.py
+++ demos/cta_arbitrage_bt/runBT.py
@@ -5,6 +5,20 @@ import sys
 sys.path.append('../Strategies')
 from T1 import StraT1
 
+from wtpy.monitor import WtBtSnooper
+from wtpy import WtDtServo
+def testBtSnooper():    
+
+    dtServo = WtDtServo()
+    # 这里配置的是基础数据文件目录
+    dtServo.setBasefiles(folder='../common/')
+
+    # 这里配置的是datakit落地的数据目录
+    dtServo.setStorage(path='../storage/')
+
+    snooper = WtBtSnooper(dtServo)
+    snooper.run_as_server(port=8081, host='localhost')
+    
 if __name__ == "__main__":
     #创建一个运行环境，并加入策略
     engine = WtBtEngine(EngineType.ET_CTA)
@@ -20,5 +34,9 @@ if __name__ == "__main__":
     analyst.add_strategy("t1_rb_i", folder="./outputs_bt/", init_capital=350000, rf=0.02, annual_trading_days=240)
     analyst.run_new()
 
-    kw = input('press any key to exit\n')
+    # kw = input('press any key to exit\n')
     engine.release_backtest()
+    
+    testBtSnooper()
+    # 运行了服务以后，在浏览器打开以下网址即可使用
+    # http://127.0.0.1:8081/backtest/backtest.html
--- demos/test_datafactory/test_datafactory.py
+++ demos/test_datafactory/test_datafactory.py
@@ -8,9 +8,9 @@ from wtpy.WtCoreDefs import WTSBarStruct
 from wtpy.apps.datahelper import DHFactory as DHF
 from wtpy.apps.datahelper.db import MysqlHelper
 
-asset = ['SSE.STK.688001',
-         'SSE.STK.688002',
-         'SSE.STK.688003']
+# asset = ['SSE.STK.688001',
+#          'SSE.STK.688002',
+#          'SSE.STK.688003']
 period = 'min5'
 
 # baostock
@@ -26,25 +26,27 @@ hlper.auth()
 # hlper.auth(**{"username":"00000000", "password":"0000000"})
 
 # 落地股票列表
-# hlper.dmpCodeListToFile('../common/stocks.json')
+hlper.dmpCodeListToFile('../common/stocks.json'); print("stock list acquired")
+
 # 下载复权因子 (only stk)
-hlper.dmpAdjFactorsToFile(codes=asset, filename='../common/adjfactors.json')
+hlper.dmpAdjFactorsToFile(codes='../common/stocks.json', filename='../common/adjfactors.json'); print("adj factor list acquired")
+
 
 # 下载K线数据
-hlper.dmpBarsToFile(folder='../storage/csv/', codes=asset, period=period); print(f'{period} dumped')
+hlper.dmpBarsToFile(folder='../storage/csv/', codes='../common/stocks.json', period=period); print(f'{period} dumped')
 # hlper.dmpBarsToFile(folder='../storage/csv/', codes=asset, period='min5'); print('min5 dumped')
 # hlper.dmpBarsToFile(folder='../storage/csv/', codes=asset, period='min1'); print('min1 dumped')
 
 # 初始化数据库
-dbHelper = MysqlHelper.MysqlHelper("localhost","chuyin","bj721006","market", 3306)
-dbHelper.initDB()
+# dbHelper = MysqlHelper.MysqlHelper("localhost","chuyin","bj721006","market", 3306)
+# dbHelper.initDB()
 
 # 将数据下载到数据库
 # hlper.dmpBarsToDB(dbHelper, codes=asset, period=period); print('kline saved to MySql')
 # hlper.dmpAdjFactorsToDB(dbHelper, codes=asset); print('adj_factor saved to MySql')
 
-dtHelper = WtDataHelper()
-dtHelper.trans_csv_bars(csvFolder='../storage/csv/', binFolder='../storage/bin/', period=period)
+# dtHelper = WtDataHelper()
+# dtHelper.trans_csv_bars(csvFolder='../storage/csv/', binFolder='../storage/bin/', period=period)
 
 # 将数据直接落地成dsb
 def on_bars_block(exchg:str, stdCode:str, firstBar:POINTER(WTSBarStruct), count:int, period:str):
@@ -73,7 +75,7 @@ def on_bars_block(exchg:str, stdCode:str, firstBar:POINTER(WTSBarStruct), count:
     dtHelper.store_bars(filename, firstBar, count, period)
     pass
 # not in tushare
-hlper.dmpBars(codes=asset, cb=on_bars_block, 
+hlper.dmpBars(codes='../common/stocks.json', cb=on_bars_block, 
               #start_date=datetime.datetime(2019,1,1), 
               #end_date=datetime.datetime(2024,1,1), 
               period=period)
